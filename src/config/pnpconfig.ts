fc99d640fd73365ecc61f8459b24e68e73053ee1
anapeksha <anapeksha.mukherjee@gmail.com>
2024-02-06 22:52:41 +0530

    graph setup

--- src/config/pnpconfig.ts
+++ src/config/pnpconfig.ts
@@ -1,4 +1,7 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { graphfi, GraphFI, SPFx as GraphSPFx } from "@pnp/graph";
+import "@pnp/graph/calendars";
+import "@pnp/graph/users";
 import { spfi, SPFI, SPFx } from "@pnp/sp";
 import "@pnp/sp/batching";
 import "@pnp/sp/items";
@@ -8,10 +11,18 @@ import "@pnp/sp/site-users/web";
 import "@pnp/sp/webs";
 
 let _sp: SPFI;
+let _graph: GraphFI;
 
 export const getSP = (context?: WebPartContext): SPFI => {
   if (!!context) {
     _sp = spfi().using(SPFx(context));
   }
   return _sp;
+};
+
+export const getGraph = (context?: WebPartContext): GraphFI => {
+  if (!!context) {
+    _graph = graphfi().using(GraphSPFx(context));
+  }
+  return _graph;
 };
