1b0737c713b0ccba1d7113c77b40da50869d73c4
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2024-02-07 20:16:33 +0530

    New webpart added

---
+++ src/webparts/calendar/CalendarWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from "@microsoft/sp-core-library";
+import {
+  PropertyPaneTextField,
+  type IPropertyPaneConfiguration,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+import * as strings from "CalendarWebPartStrings";
+import { ICalendarProps } from "../../types";
+import CalendarDisplay from "./CalendarDisplay";
+
+export interface ICalendarWebPartProps extends ICalendarProps {}
+
+export default class CalendarWebPart extends BaseClientSideWebPart<ICalendarWebPartProps> {
+  public render(): void {
+    const element: React.ReactElement<ICalendarProps> = React.createElement(
+      CalendarDisplay,
+      {
+        context: this.context,
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected async onInit(): Promise<void> {
+    await super.onInit();
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription,
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
