a5ce0711adcba4ad3534b60f26f8af8cf3bc99a2
anapeksha <anapeksha.mukherjee@carrier.com>
2024-06-07 19:49:47 +0530

    using spTheme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,8 +1,20 @@
 import { createTheme, colors } from '@mui/material';
 
+const spTheme = (window as any).__themeState__.theme;
+
 const theme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      light: spTheme.themeLight,
+      main: spTheme.themePrimary,
+      dark: spTheme.themeDark,
+    },
+    secondary: {
+      light: spTheme.themeLighterAlt,
+      main: spTheme.themeSecondary,
+      dark: spTheme.themeDarkerAlt,
+    },
   },
   components: {
     MuiFormLabel: {
