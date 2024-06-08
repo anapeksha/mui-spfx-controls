0a636cedde2c0dc0682ebb2e441f28455dfa6be6
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2024-06-08 16:26:33 +0530

    props added

--- src/types/IDashboardProps.ts
+++ src/types/IDashboardProps.ts
@@ -1,11 +1,13 @@
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { BoxProps } from '@mui/material';
+import { DataGridProps } from '@mui/x-data-grid';
 
 interface IDashboardProps {
   context: WebPartContext;
   list: string;
   fields: string[];
   height?: BoxProps['height'];
+  sx?: DataGridProps['sx'];
 }
 
 export type { IDashboardProps };
--- src/webparts/dashboard/DashboardDisplay.tsx
+++ src/webparts/dashboard/DashboardDisplay.tsx
@@ -1,15 +1,15 @@
-import { Dashboard } from "../../components";
 import { ThemeProvider } from '@mui/material';
 import * as React from 'react';
+import { Dashboard } from '../../components';
 import { theme } from '../../config';
-import { IDashboardProps } from "../../types";
+import { IDashboardProps } from '../../types';
 
 const DashboardDisplay: React.FC<IDashboardProps> = (props) => {
   return (
     <ThemeProvider theme={theme}>
-        <Dashboard {...props}/>
+      <Dashboard {...props} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default DashboardDisplay
+export default DashboardDisplay;
