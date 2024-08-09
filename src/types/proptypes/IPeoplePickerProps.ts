ce64abd2afb5e2dd69ca0a6854320495da1b3bf9
anapeksha <anapeksha.mukherjee@carrier.com>
2024-08-09 12:50:47 +0530

    type architecture modified

---
+++ src/types/proptypes/IPeoplePickerProps.ts
@@ -0,0 +1,50 @@
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+import {
+  AutocompleteProps,
+  ChipOwnProps,
+  TextFieldProps,
+  TextFieldVariants,
+} from '@mui/material';
+import { CSSProperties, ReactNode } from 'react';
+import { IExtendedPeoplePickerEntity } from '../IExtendedPeoplePicker';
+
+type AutocompleteBaseProps = AutocompleteProps<
+  IExtendedPeoplePickerEntity | IExtendedPeoplePickerEntity[],
+  boolean,
+  boolean,
+  true
+>;
+
+interface IPeoplePickerBaseProps {
+  context: WebPartContext;
+  label: string;
+  multiple?: boolean;
+  required?: boolean;
+  searchSuggestionLimit?: number;
+  personSelectionLimit?: number;
+  disabled?: boolean;
+  variant?: TextFieldVariants;
+  tagVariant?: ChipOwnProps['variant'];
+  color?: TextFieldProps['color'];
+  tagColor?: ChipOwnProps['color'];
+  size?: AutocompleteBaseProps['size'];
+  styles?: CSSProperties;
+  sx?: AutocompleteBaseProps['sx'];
+  name?: TextFieldProps['name'];
+  fullWidth?: TextFieldProps['fullWidth'];
+  LoadingComponent?: ReactNode;
+}
+
+interface ISingleValueProps extends IPeoplePickerBaseProps {
+  multiple: true;
+  defaultValue?: IExtendedPeoplePickerEntity[];
+  onSelectionChange?: (value: IExtendedPeoplePickerEntity[]) => void;
+}
+
+interface IMultiValueProps extends IPeoplePickerBaseProps {
+  multiple?: false;
+  defaultValue?: IExtendedPeoplePickerEntity;
+  onSelectionChange?: (value: IExtendedPeoplePickerEntity) => void;
+}
+
+export type IPeoplePickerProps = ISingleValueProps | IMultiValueProps;
