8791c2a27a10aca4f6e13b3e0292352154c3da10
anapeksha <anapeksha.mukherjee@carrier.com>
2024-08-20 17:55:51 +0530

    organize imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,23 @@
 import Dashboard from './Dashboard/Dashboard';
-import ListForm from './ListForm/ListForm';
-import ListItemPicker from './ListItemPicker/ListItemPicker';
-import Navigation from './Navigation/Navigation';
-import PeoplePicker from './PeoplePicker/PeoplePicker';
 import type { IDashboardProps } from './Dashboard/IDashboardProps';
 import type { IListFormProps } from './ListForm/IListFormProps';
+import ListForm from './ListForm/ListForm';
 import type { IListItemPickerProps } from './ListItemPicker/IListItemPickerProps';
+import ListItemPicker from './ListItemPicker/ListItemPicker';
 import type { INavigationProps } from './Navigation/INavigationProps';
+import Navigation from './Navigation/Navigation';
 import type { IPeoplePickerProps } from './PeoplePicker/IPeoplePickerProps';
+import PeoplePicker from './PeoplePicker/PeoplePicker';
 
 export {
   Dashboard,
-  type IDashboardProps,
   ListForm,
-  type IListFormProps,
   ListItemPicker,
-  type IListItemPickerProps,
   Navigation,
-  type INavigationProps,
   PeoplePicker,
+  type IDashboardProps,
+  type IListFormProps,
+  type IListItemPickerProps,
+  type INavigationProps,
   type IPeoplePickerProps,
 };
