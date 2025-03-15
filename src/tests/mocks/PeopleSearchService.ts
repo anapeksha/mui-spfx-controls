f9fdd1eca266463d8414bcece052400094500ccf
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-15 14:43:34 +0530

    multiple mock users

--- src/tests/mocks/PeopleSearchService.ts
+++ src/tests/mocks/PeopleSearchService.ts
@@ -9,7 +9,43 @@ export const mockUser = {
   Image: 'https://example.com/profile.jpg',
 } as IPeoplePickerEntity;
 
-export const mockUserList: IPeoplePickerEntity[] = [mockUser];
+export const mockUsers = [
+  {
+    Key: 'mock-user-key-1',
+    DisplayText: 'John Doe',
+    Description: 'john.doe@example.com',
+    EntityData: { Email: 'john.doe@example.com', PrincipalType: 'User' },
+    Image: 'https://example.com/profile1.jpg',
+  },
+  {
+    Key: 'mock-user-key-2',
+    DisplayText: 'Jane Smith',
+    Description: 'jane.smith@example.com',
+    EntityData: { Email: 'jane.smith@example.com', PrincipalType: 'User' },
+    Image: 'https://example.com/profile2.jpg',
+  },
+  {
+    Key: 'mock-user-key-3',
+    DisplayText: 'Michael Johnson',
+    Description: 'michael.johnson@example.com',
+    EntityData: { Email: 'michael.johnson@example.com', PrincipalType: 'User' },
+    Image: 'https://example.com/profile3.jpg',
+  },
+  {
+    Key: 'mock-user-key-4',
+    DisplayText: 'Emily Davis',
+    Description: 'emily.davis@example.com',
+    EntityData: { Email: 'emily.davis@example.com', PrincipalType: 'User' },
+    Image: 'https://example.com/profile4.jpg',
+  },
+  {
+    Key: 'mock-user-key-5',
+    DisplayText: 'Robert Brown',
+    Description: 'robert.brown@example.com',
+    EntityData: { Email: 'robert.brown@example.com', PrincipalType: 'User' },
+    Image: 'https://example.com/profile5.jpg',
+  },
+] as IPeoplePickerEntity[];
 
 export class PeopleSearchService {
   constructor(context: WebPartContext) {}
@@ -19,7 +55,7 @@ export class PeopleSearchService {
     query: string,
     maximumSuggestions?: number
   ): Promise<IPeoplePickerEntity[]> {
-    return Promise.resolve(mockUserList);
+    return Promise.resolve(mockUsers);
   }
 
   public async resolveUser(
