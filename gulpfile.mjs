7c059fe63499fa6ad4692f98c5616a0e337fe831
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-18 17:55:17 +0530

    remove unused code

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,13 +1,8 @@
 'use strict';
 
 import build from '@microsoft/sp-build-web';
-import { exec } from 'child_process';
-import { error, log } from 'console';
 import gulp from 'gulp';
 import * as fastServe from 'spfx-fast-serve-helpers';
-import util from 'util';
-
-const execPromise = util.promisify(exec);
 
 build.addSuppression(
   `Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`
@@ -21,15 +16,5 @@ build.rig.getTasks = function () {
 };
 
 fastServe.addFastServe(build);
-
-gulp.task('build:release', async () => {
-  try {
-    log('Building modules...');
-    await execPromise('tsc -b includes/tsconfig.build.json');
-    log('Build complete.');
-  } catch (err) {
-    error(err);
-  }
-});
 
 build.initialize(gulp);
