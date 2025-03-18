aec96c8799d57cc9d46382faa283b0a1c429e50e
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-18 17:16:08 +0530

    Ref object forwarded

--- src/components/SiteBreadcrumb/SiteBreadcrumb.tsx
+++ src/components/SiteBreadcrumb/SiteBreadcrumb.tsx
@@ -1,43 +1,47 @@
 import { Breadcrumbs, Link } from '@mui/material';
 import { Logger } from '@pnp/logging';
-import React, { useEffect, useState } from 'react';
+import React, {
+  forwardRef,
+  ForwardRefExoticComponent,
+  RefObject,
+  useEffect,
+  useState,
+} from 'react';
 import { LinkItems, SiteService } from '../../services/SiteService';
 import { ISiteBreadcrumbProps } from './ISiteBreadcrumbProps';
 
-const SiteBreadcrumb: React.FC<ISiteBreadcrumbProps> = ({
-  context,
-  ...breadcrumbProps
-}) => {
-  const siteService = new SiteService(context);
-  const [breadcrumbData, setBreadcrumbData] = useState<LinkItems[] | undefined>(
-    []
-  );
+const SiteBreadcrumb: ForwardRefExoticComponent<ISiteBreadcrumbProps> =
+  forwardRef(({ context, ...breadcrumbProps }, ref: RefObject<HTMLElement>) => {
+    const siteService = new SiteService(context);
+    const [breadcrumbData, setBreadcrumbData] = useState<
+      LinkItems[] | undefined
+    >([]);
 
-  useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const generatedBreadcrumbData =
-          siteService.generateBreadcrumbs(context);
-        console.log(generatedBreadcrumbData);
-        setBreadcrumbData(generatedBreadcrumbData);
-      } catch (error) {
-        Logger.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+      const fetchData = async (): Promise<void> => {
+        try {
+          const generatedBreadcrumbData =
+            siteService.generateBreadcrumbs(context);
+          console.log(generatedBreadcrumbData);
+          setBreadcrumbData(generatedBreadcrumbData);
+        } catch (error) {
+          Logger.error(error);
+        }
+      };
+      fetchData();
+    }, []);
 
-  return (
-    <Breadcrumbs {...breadcrumbProps} aria-label="breadcrumb">
-      {breadcrumbData
-        ? breadcrumbData.map(({ label, href, key }) => (
-            <Link href={href} key={key}>
-              {label}
-            </Link>
-          ))
-        : null}
-    </Breadcrumbs>
-  );
-};
+    return (
+      <Breadcrumbs {...breadcrumbProps} aria-label="breadcrumb" ref={ref}>
+        {breadcrumbData
+          ? breadcrumbData.map(({ label, href, key }) => (
+              <Link href={href} key={key}>
+                {label}
+              </Link>
+            ))
+          : null}
+      </Breadcrumbs>
+    );
+  });
 
 export default SiteBreadcrumb;
