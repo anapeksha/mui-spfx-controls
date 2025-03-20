98cb860f59a4e843d27471be257fa6aaaaf752e3
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-20 23:05:35 +0530

    new props

--- src/components/SiteBreadcrumb/ISiteBreadcrumbProps.ts
+++ src/components/SiteBreadcrumb/ISiteBreadcrumbProps.ts
@@ -1,8 +1,26 @@
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { BreadcrumbsProps } from '@mui/material';
+import { ReactNode } from 'react';
+import { LinkItem } from '../../services/SiteService';
 
 interface ISiteBreadcrumbProps extends BreadcrumbsProps {
+  /**
+   * Webpart Context
+   */
   context: WebPartContext;
+
+  /**
+   * Render first breadcrumb
+   */
+  renderFirstItem?: (data: LinkItem) => ReactNode;
+  /**
+   * Render other breadcrumbs
+   */
+  renderItem?: (data: LinkItem) => ReactNode;
+  /**
+   * Render last breadcrumb
+   */
+  renderLastItem?: (data: LinkItem) => ReactNode;
 }
 
 export type { ISiteBreadcrumbProps };
