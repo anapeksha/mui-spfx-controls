b4a840aeff079a48b7712d2ebdc298e3331d36c0
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-20 23:05:24 +0530

    SiteService modified

--- src/services/SiteService.ts
+++ src/services/SiteService.ts
@@ -1,9 +1,8 @@
-import { Environment, EnvironmentType } from '@microsoft/sp-core-library';
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { SPFI } from '@pnp/sp';
 import { getSp } from '../config/pnp.config';
 
-interface LinkItems {
+interface LinkItem {
   label: string;
   key: string;
   href: string;
@@ -12,7 +11,8 @@ interface LinkItems {
 
 class SiteService {
   private sp: SPFI;
-  private _linkItems: LinkItems[] = [];
+  private _linkItems: LinkItem[] = [];
+
   constructor(context: WebPartContext) {
     this.sp = getSp(context);
   }
@@ -25,10 +25,10 @@ class SiteService {
       const response = await this.sp.web.getParentWeb().then((response) => {
         return response.select('Id', 'Title', 'ServerRelativeUrl')();
       });
-      console.log(response);
-      if (!response.ServerRelativeUrl && !response.Title) {
+      if (!response?.ServerRelativeUrl || !response?.Title) {
         return;
       }
+
       this._linkItems.unshift({
         label: response.Title,
         key: response.Id,
@@ -40,21 +40,25 @@ class SiteService {
         context.pageContext.site.serverRelativeUrl
       ) {
         return;
-      } else {
-        webUrl = webUrl.substring(
-          0,
-          webUrl.indexOf(`${response.ServerRelativeUrl}/`) +
-            response.ServerRelativeUrl.length
-        );
-        return this.getParentWeb(context, webUrl);
       }
+
+      const newWebUrl = webUrl.substring(
+        0,
+        webUrl.indexOf(`${response.ServerRelativeUrl}/`) +
+          response.ServerRelativeUrl.length
+      );
+
+      await this.getParentWeb(context, newWebUrl);
     } catch (error) {
-      /* empty */
-      console.log(error);
+      console.error('Error fetching parent web:', error);
     }
   }
 
-  public generateBreadcrumbs(context: WebPartContext): LinkItems[] | undefined {
+  public async generateBreadcrumbData(
+    context: WebPartContext
+  ): Promise<LinkItem[]> {
+    this._linkItems = [];
+
     this._linkItems.push({
       label: context.pageContext.web.title,
       key: context.pageContext.web.id.toString(),
@@ -63,20 +67,15 @@ class SiteService {
         !!context.pageContext.list &&
         !context.pageContext.list.serverRelativeUrl,
     });
+
     if (
-      context.pageContext.site.serverRelativeUrl ===
+      context.pageContext.site.serverRelativeUrl !==
       context.pageContext.web.serverRelativeUrl
     ) {
-      return this._linkItems;
-    } else {
-      if (Environment.type === EnvironmentType.Test) {
-        console.log(this._linkItems);
-        return this._linkItems;
-      } else {
-        this.getParentWeb(context, context.pageContext.web.absoluteUrl);
-      }
+      await this.getParentWeb(context, context.pageContext.web.absoluteUrl);
     }
+    return this._linkItems;
   }
 }
 
-export { SiteService, type LinkItems };
+export { SiteService, type LinkItem };
