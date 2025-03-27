c6c6c8214ca3cf86187cb0358a79318fac901e1e
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-28 01:59:58 +0530

    localizations

---
+++ src/webparts/codeEditor/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICodeEditorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  RenderInputFieldLabel: string;
+}
+
+declare module 'CodeEditorWebPartStrings' {
+  const strings: ICodeEditorWebPartStrings;
+  export = strings;
+}
