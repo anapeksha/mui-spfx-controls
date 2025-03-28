e42c9bdf445bd3c04f5d2cb1a38659730342d2a2
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-03-28 13:47:44 +0530

    tab size control added

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -1,15 +1,16 @@
-/* eslint-disable-file @typescript-eslint/no-duplicate-enum-values */
 import { Editor } from '@monaco-editor/react';
 import {
   Box,
   FormControlLabel,
   MenuItem,
+  Paper,
   Switch,
   TextField,
 } from '@mui/material';
 import React, {
   forwardRef,
   ForwardRefExoticComponent,
+  RefObject,
   useMemo,
   useState,
 } from 'react';
@@ -64,10 +65,14 @@ const themes: Array<IThemeArray> = [
 ];
 
 const CodeEditor: ForwardRefExoticComponent<ICodeEditorProps> = forwardRef(
-  ({ height, value, renderControls = true, onChange, ...props }, ref) => {
+  (
+    { height, value, renderControls = true, onChange, ...props },
+    ref: RefObject<HTMLDivElement>
+  ) => {
     const [language, setLanguage] = useState<Languages>(Languages.javascript);
     const [theme, setTheme] = useState<Theme>(Theme.vs);
     const [fontSize, setFontSize] = useState(14);
+    const [tabSize, setTabSize] = useState(4);
     const [wordWrap, setWordWrap] = useState<'off' | 'on'>('off');
     const [lineNumbers, setLineNumbers] = useState<'on' | 'off'>('on');
     const [minimap, setMinimap] = useState<{
@@ -85,13 +90,26 @@ const CodeEditor: ForwardRefExoticComponent<ICodeEditorProps> = forwardRef(
         wordWrap,
         lineNumbers,
         minimap,
+        tabSize,
       };
-    }, [theme, fontSize, wordWrap, lineNumbers, minimap]);
+    }, [theme, fontSize, wordWrap, lineNumbers, minimap, tabSize]);
 
     return (
-      <Box sx={{ height: height ?? { xs: 250, md: 500, xl: 750 } }} ref={ref}>
+      <Paper
+        variant="outlined"
+        sx={{
+          height: height ?? { xs: 300, md: 400, xl: 500 },
+          p: 2,
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          overflow: 'hidden',
+        }}
+        ref={ref}
+      >
         {renderControls ? (
-          <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <Box sx={{ width: 150 }}>
               <TextField
                 id="code-editor-language"
@@ -143,6 +161,19 @@ const CodeEditor: ForwardRefExoticComponent<ICodeEditorProps> = forwardRef(
                 }}
               />
             </Box>
+            <Box sx={{ width: 150 }}>
+              <TextField
+                id="code-editor-tab-size"
+                value={tabSize}
+                type="number"
+                label="Tab Size"
+                fullWidth
+                size="small"
+                onChange={(event) => {
+                  setTabSize(Number(event.target.value));
+                }}
+              />
+            </Box>
             <FormControlLabel
               control={
                 <Switch
@@ -178,8 +209,10 @@ const CodeEditor: ForwardRefExoticComponent<ICodeEditorProps> = forwardRef(
             />
           </Box>
         ) : null}
-        <Editor {...props} language={language} options={options} />
-      </Box>
+        <Box sx={{ flexGrow: 1, overflow: 'hidden' }}>
+          <Editor {...props} language={language} options={options} />
+        </Box>
+      </Paper>
     );
   }
 );
