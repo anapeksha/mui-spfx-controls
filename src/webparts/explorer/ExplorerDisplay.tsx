c50d353be90ebb967d3c16124ed903c4fbd37076
anapeksha <anapeksha.mukherjee@gmail.com>
2026-01-08 06:21:28 +0530

    Fix build problems

--- src/webparts/explorer/ExplorerDisplay.tsx
+++ src/webparts/explorer/ExplorerDisplay.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import Explorer, { IExplorerProps } from '../../components/Explorer';
+import { Explorer, IExplorerProps } from '../../components/Explorer';
 import { theme } from '../../config/theme.config';
 
 const ExplorerDisplay: React.FC<IExplorerProps> = (props) => {
